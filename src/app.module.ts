107c62ef0ae31e2abe2bee1f2a646ac9943da334
MarinhoCM <marinhoc384@gmail.com>
2026-01-14 17:06:15 -0300

    :recycle: refactor: reestruturando projeto para utilização de monorepo

--- src/app.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { InfraModule } from './infra/infra.module';
-
-@Module({
-  imports: [
-    InfraModule
-  ],
-})
-export class AppModule { }
--- src/common/constraints.ts
+++
@@ -1,2 +0,0 @@
-export const ORDER_SERVICE_RABBITMQ = 'ORDER_MS_SERVICE'
-export const ORDER_QUEUE_RABBITMQ = 'order.ms.queue'
